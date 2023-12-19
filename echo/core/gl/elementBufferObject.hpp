0c5aca11bde1cc6f1f0b5122bd40c7c53076731a
Mateus R. Culpo <mateusculpo@gmail.com>
2023-12-19 00:51:54 -0300

    Feat: implementation the Element Buffer Object (EBO) Date and Code

---
+++ echo/core/gl/elementBufferObject.hpp
@@ -0,0 +1,31 @@
+#ifndef OPENGE_ELEMENT_BUFFER_OBJECT
+#define OPENGE_ELEMENT_BUFFER_OBJECT
+#include <core/core.h>
+namespace openge::gl::ebo {
+
+	struct ElementBufferObject {
+		unsigned int mRendererID;
+	};
+
+#ifndef OPENGE_ELEMENT_BUFFER_OBJECT_MANAGER
+#define OPENGE_ELEMENT_BUFFER_OBJECT_MANAGER
+	void create(ElementBufferObject* pEbo, unsigned int size, const void* indices, unsigned int glEnum) {
+		glGenBuffers(1, &pEbo->mRendererID);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, pEbo->mRendererID);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, indices, glEnum);
+	}
+
+	void remove(ElementBufferObject* pEbo) {
+		glDeleteBuffers(1, &pEbo->mRendererID);
+	}
+
+	void bind(ElementBufferObject* pEbo) {
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, pEbo->mRendererID);
+	}
+
+	void unbind() {
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	}
+#endif
+}
+#endif
